4a2525db1605ef581ecd764b6e842e78d82ba651
Alf Eaton <alf.eaton@mendeley.com>
2012-01-03 12:06:56 +0000

    initial import of Alfbiance

---
+++ Alfbiance/metacity-1/gconf-settings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gconftool -s --type string /apps/metacity/general/button_layout "close:minimize"
+gconftool -s --type bool /apps/panel/toplevels/panel_0/background/stretch "True"
+gconftool -s --type bool /apps/panel/toplevels/top_panel_screen0/background/stretch "True"
+gconftool -s --type bool /desktop/gnome/interface/hide_decorator_tooltip "TRUE"
+
